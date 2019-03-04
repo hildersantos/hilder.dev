5007a5cfe8d565248d15d950911b9c38c58f7645
Hilder Santos <hilder@hildersantos.com>
2019-03-04 02:48:51 -0300

    WIP configure posts and pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hilder Santos`,
+    description: `Blog pessoal do Hilder Santos`,
+    author: `@hildersantos`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,30 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content`,
+        name: "pages",
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/src/static/media`,
+    //     name: "media",
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "assets",
+    //     path: `${__dirname}/static`,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
   ],
 }
---
+++ src/content/pages/my-new-post/index.md
@@ -0,0 +1,10 @@
+---
+title: A good title
+description: A new description
+date: 2019-03-04 02:39:13
+image:
+draft: false
+template: post
+---
+
+Here we go...
