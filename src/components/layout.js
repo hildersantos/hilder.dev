93e671893784ca3cd60efdf1d4050884592fcefb
Hilder Santos <hilder@hildersantos.com>
2019-03-04 11:54:54 -0300

    Header implemented

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,22 +30,12 @@ const Layout = ({ children, title, description }) => (
           title={title || data.site.siteMetadata.title}
           description={description || undefined}
         />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <main className="main">
+          <div className="container">
+            <Header siteTitle={data.site.siteMetadata.title} />
+            {children}
+          </div>
+        </main>
       </>
     )}
   />
