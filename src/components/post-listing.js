12d51e609f183f971f0931627945867cdd18d22a
Hilder Santos <hilder@hildersantos.com>
2019-03-05 00:23:07 -0300

    Post listing updated with cosmetic changes

--- src/components/post-listing.js
+++ src/components/post-listing.js
@@ -1,16 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
+import moment from "moment"
 
 const PostListing = ({ edges }) => {
   return (
     <section className="post-listing">
       {edges.map(({ node }) => (
         <article className="post-listing__article" key={node.id}>
-          <h2 className="post-listing__title">
-            <Link to={node.fields.slug} className="post-listing__link">
-              {node.frontmatter.title}
-            </Link>
-          </h2>
+          <Link to={node.fields.slug} className="post-listing__link">
+            <h2 className="post-listing__title">{node.frontmatter.title}</h2>
+            <time
+              className="post-listing__date"
+              dateTime={moment(node.frontmatter.date).format()}
+            >
+              {moment(node.frontmatter.date).format("DD [de] MMMM [de] YYYY")}
+            </time>
+            <div className="post-listing__description">
+              <p>{node.frontmatter.description}</p>
+            </div>
+          </Link>
         </article>
       ))}
     </section>
